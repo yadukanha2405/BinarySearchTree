8721a10690bf508e110352ea9d9829495cdd4112
Kanhaiya Yadav <101487969+yadukanha2405@users.noreply.github.com>
2022-08-03 00:00:25 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  add(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      var current = this.root;
+      while (true) {
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+  find(value) {
+    if (this.root === null) return false;
+    var current = this.root;
+    var found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    return found;
+  }
+}
+
+const result = new BST();
+result.add(10);
+result.add(5);
+result.add(13);
+result.add(11);
+result.add(2);
+result.add(16);
+result.add(7);
+
+console.log(result.find(11));
